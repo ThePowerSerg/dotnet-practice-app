925143dff1a17e188b7a33cd30bbdb8d1bf0d2f7
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-07 12:03:16 -0400

    complete setup for email service

--- MessagingApp/Program.cs
+++ MessagingApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MessagingApp.Controllers;
+using MessagingApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+// 1. Set up the DI container
+var services = new ServiceCollection();
+
+// Register the interface and its implementation
+services.AddTransient<IMessageService, EmailService>();
+
+// Register the consumer class
+services.AddTransient<MessageController>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+// 2. Resolve and run
+var manager = serviceProvider.GetRequiredService<MessageController>();
+manager.Send(); // Output: Hello from the Email Service!
--- MessagingApp/Services/IMessageService.cs
+++ MessagingApp/Services/IMessageService.cs
@@ -3,6 +3,5 @@ namespace MessagingApp.Services
     public interface IMessageService
     {
         string GetMessage();
-        //string SendMessage();
     }
 }
