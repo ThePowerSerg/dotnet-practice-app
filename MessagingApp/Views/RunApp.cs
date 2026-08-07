286bbb8bfbcb3524a843c6c67b2bed45ada33eae
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-07 17:23:18 -0400

    Add comments to RunApp

--- MessagingApp/Views/RunApp.cs
+++ MessagingApp/Views/RunApp.cs
@@ -5,6 +5,7 @@ namespace MessagingApp.Views
 {
     public class RunApp
     {
+        // DI pattern ?
         private readonly MessageController controller;
 
         public RunApp(MessageController controller)
@@ -25,5 +26,13 @@ namespace MessagingApp.Views
             controller.SendEmail(user, "Keep it flexible and DI via Email!");
             controller.SendSMS(user, "Keep it flexible and DI via SMS");
         }
+
+        /*
+           TODO - create an interface:
+           1. Get the name of a user. 
+           2. Send an email.
+           3. Send an SMS. 
+        */
+        
     }
 }
