b00bb71baa069a8f6a03bca012c3939643a91a83
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-08 13:51:36 -0400

    finalized DB Initalizer logic

--- MessagingApp/Data/DbInitializer.cs
+++ MessagingApp/Data/DbInitializer.cs
@@ -15,13 +15,11 @@ namespace MessagingApp.Data
             var userProfiles = new List<UserProfile>()
             {
                 new() {
-                    Id = 1,
                     UserName = "sergferreira81",
                     Email = "sergferreira81@gmail.com",
                     PhoneNumber = "7817332393"
                 },
                 new() {
-                    Id = 2,
                     UserName = "sergiof810",
                     Email = "sergiof810@outlook.com",
                     PhoneNumber = "7817332393"
