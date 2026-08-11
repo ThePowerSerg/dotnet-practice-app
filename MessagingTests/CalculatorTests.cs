8b8d69a6c78551c4ca3b756567d09ed48a24f1ea
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-11 15:04:04 -0400

    add docs, test project references and fiest test

---
+++ MessagingTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using MessagingApp.Services;
+
+namespace MessagingTests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void IsEvenTest()
+    {
+        var calculator = new CalculatorService();
+
+        int x = 1;
+        int y = 2;
+
+        var xResult = calculator.IsEven(x);
+
+        var yResult = calculator.IsEven(y);
+
+        Assert.False(xResult);
+        Assert.True(yResult);
+    }
+}
