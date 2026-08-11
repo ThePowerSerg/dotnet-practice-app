1db4ba2e291513b079cf7cec34feba05aebf01ee
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-11 16:14:23 -0400

    Initial tests

--- Docs/Concepts/TESTS.md
+++ Docs/Concepts/TESTS.md
@@ -7,3 +7,14 @@
 ![alt text](image.png)
 
 ![alt text](image-1.png)
+
+The `[Fact]` attribute declares a test method that's run by the test runner. From the PrimeService.Tests folder, run dotnet test
+
+`Fact` 
+
+`[Theory]` represents a suite of tests that execute the same code but have different input arguments.
+
+`Theory` 
+- [InlineData]
+- [MemberData]
+- [ClassData]
--- MessagingTests/CalculatorTests.cs
+++ MessagingTests/CalculatorTests.cs
@@ -4,6 +4,7 @@ namespace MessagingTests;
 
 public class CalculatorTests
 {
+    // Test IsEven
     [Fact]
     public void IsEvenTest()
     {
@@ -19,4 +20,49 @@ public class CalculatorTests
         Assert.False(xResult);
         Assert.True(yResult);
     }
+
+    // Test DiffTest
+    [Theory]
+    [InlineData(1, 2, 1)]
+    [InlineData(2, 4, 2)]
+    public void DiffTest(int x, int y, int expectedValue)
+    {
+        var calculator = new CalculatorService();
+        var result = calculator.Diff(x, y);
+        Assert.Equal(expectedValue, result);
+    }
+
+    // Test Add
+    [Theory]
+    [InlineData(15, 10, 5)]
+    public void AddTest(int expectedValue, int x, int y)
+    {
+        var calculator = new CalculatorService();
+        var result = calculator.Add(x, y);
+        Assert.Equal(expectedValue, result);
+    }
+
+    // Test Add
+    [Theory]
+    [InlineData(8, 8, 8)]
+    public void AverageTest(int expectedValue, int x, int y)
+    {
+        var calculator = new CalculatorService();
+        var result = calculator.Average(x, y);
+        Assert.Equal(expectedValue, result);
+    }
+
+
+    // Sum Test
+    [Theory]
+    [InlineData(7, 4, 3)]
+    [InlineData(-5, -3, -2)]
+    public void SumTest(int expectedValue, params int[] values)
+    {
+        var calculator = new CalculatorService();
+        var result = calculator.Sum(values);
+        Assert.Equal(expectedValue, result);
+    }
 }
+
+
