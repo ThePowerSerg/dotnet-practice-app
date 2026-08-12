c4aba57d2816f898e07a46f9ed6743b07fe90fe7
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-12 13:42:49 -0400

    Add Book service to API

---
+++ MessagingAPI/Services/IBookService.cs
@@ -0,0 +1,12 @@
+using MessagingAPI.Models;
+
+namespace MessagingAPI.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetBooks();
+        Book AddBook(Book book);
+        Book GetBookById(Guid Id);
+        void RemoveBook(Guid id);
+    }
+}
