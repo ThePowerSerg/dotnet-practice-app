47fbb302f7a067ed63d495160f35a85039f96542
Sergio L. Ferreira <sferreir@bu.edu>
2026-08-12 14:56:32 -0400

    Add Books controller

--- MessagingAPI/Program.cs
+++ MessagingAPI/Program.cs
@@ -1,3 +1,6 @@
+using MessagingAPI.Contollers;
+using MessagingAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddTransient<IBookService, BookService>();
 
 var app = builder.Build();
 
